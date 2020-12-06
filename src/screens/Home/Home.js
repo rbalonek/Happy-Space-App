d4a2c8b0dd72b9940d14e2b741fa4dad7b47c2ca
bob <robertbalonek@gmail.com>
2020-12-06 13:42:08 -0800

    Buttons and Color Scheme

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,30 +1,155 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
+const HomeScreen = ({ navigation }) => {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+
+  useEffect(() => {
+    fetchApiCall();
+  }, []);
+
+  const fetchApiCall = () => {
+    fetch("https://api.quotable.io/random", {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        setQuote(response.content);
+        setAuthor(response.author);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.titleView}>
+        <Text style={styles.title}>Welcome to Happy Space</Text>
+      </View>
+      <View style={styles.quotes}>
+        <Text style={styles.quote}>"{quote}"</Text>
+        <Text style={styles.author}>-{author}</Text>
+      </View>
+      <View style={styles.allButtons}>
+        <View style={styles.buttonsTop}>
+          <TouchableOpacity
+            style={styles.button}
+            title="Go to News"
+            onPress={() => navigation.navigate("News")}
+          >
+            <Text style={styles.buttonText}>News</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.button}
+            title="This Day In History"
+            onPress={() => navigation.navigate("This Day in History")}
+          >
+            <Text style={styles.buttonText}>This Day In History</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.buttonsTop}>
+          <TouchableOpacity
+            style={styles.button}
+            title="See Dad Jokes!"
+            onPress={() => navigation.navigate("DadJokes")}
+          >
+            <Text style={styles.buttonText}>Dad Jokes</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            title="Bored?"
+            onPress={() => navigation.navigate("Bored")}
+          >
+            <Text style={styles.buttonText}>Bored?</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+};
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "#e0dcb5",
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
   },
+  titleView: {
+    backgroundColor: "#aad7e4",
+    width: "100%",
+    height: 100,
+    position: "absolute",
+    top: 0,
+    justifyContent: "center",
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: { width: 0.5, height: 1 },
+    shadowOpacity: 1.9,
+    shadowRadius: 8,
+  },
+  title: {
+    color: "#000",
+    fontSize: 25,
+    fontFamily: "Baskerville-SemiBold",
+  },
+  quotes: {
+    position: "relative",
+    top: "0%",
+    paddingLeft: 10,
+    paddingRight: 10,
+    fontFamily: "Didot",
+  },
+  quote: {
+    fontSize: 20,
+  },
+  author: {
+    color: "grey",
+  },
+  allButtons: {
+    position: "absolute",
+    bottom: "5%",
+  },
+  buttonsTop: {
+    position: "relative",
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    width: "100%",
+  },
+  buttonsBottom: {
+    position: "relative",
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    width: "100%",
+  },
+  button: {
+    backgroundColor: "#a2c6d6",
+    height: 60,
+    width: 150,
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 0.5,
+    marginBottom: 20,
+    borderBottomWidth: 4,
+    borderRadius: 5,
+    shadowOffset: { width: 5, height: 5 },
+    shadowColor: "black",
+    shadowOpacity: 1.0,
+  },
+  buttonText: {
+    color: "#000",
+    fontWeight: "bold",
+  },
 });
-
-const HomeScreen = ({ navigation }) => {
-  return (
-    <View style={styles.container}>
-      <Text>Home Screen</Text>
-      <Button title="Go to News" onPress={() => navigation.navigate("News")} />
-      <Button
-        title="See Dad Jokes!"
-        onPress={() => navigation.navigate("DadJokes")}
-      />
-      <Button title="Bored?" onPress={() => navigation.navigate("Bored")} />
-      <Button
-        title="This Day In History"
-        onPress={() => navigation.navigate("This Day in History")}
-      />
-    </View>
-  );
-};
-
 export default HomeScreen;
