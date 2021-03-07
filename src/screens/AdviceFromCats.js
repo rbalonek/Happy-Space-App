ccdb6929be830f46376a7675cd669d372159f267
bob <robertbalonek@gmail.com>
2021-03-07 10:50:52 -0800

    Cat Advice Adj position

--- src/screens/AdviceFromCats.js
+++ src/screens/AdviceFromCats.js
@@ -250,7 +250,7 @@ const styles = StyleSheet.create({
   adviceWordBubble: {
     transform: [{ scaleY: -1 }],
     position: "relative",
-    bottom: "80%",
+    bottom: "90%",
     zIndex: -1,
   },
   adviceText: {
